8f07a4bb212288751933461be9b90224022ca792
ASKARALI001 <askartazhmuradov@gmail.com>
2022-11-25 18:33:48 +0600

    Milcase project

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper(".mySwiper", {
+    scrollbar: {
+        el: ".swiper-scrollbar",
+        hide: true,
+
+    },
+
+});
+const swiperN = new Swiper('.swiper', {
+    autoplay: {
+        delay: 5000,
+    },
+});
